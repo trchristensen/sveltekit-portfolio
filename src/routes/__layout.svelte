db3873650e151be41319ae9ee27e797b2ffa6c4b
Todd Christensen <toddchristensen@protonmail.com>
2022-07-12 23:36:35 +0800

    fix: layout fixes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,61 +1,74 @@
 <script lang="ts">
-  import '../global.postcss'
-  import { theme } from './store'
-  import { page } from '$app/stores'
-  import { onMount } from 'svelte'
-  import ToggleTheme from '$lib/ToggleTheme.svelte'
-  import External from '$lib/icons/External.icon.svelte'
-import Svelte from '$lib/icons/Svelte.icon.svelte'
+	import '../global.postcss';
+	import { theme } from './store';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import ToggleTheme from '$lib/ToggleTheme.svelte';
+	import External from '$lib/icons/External.icon.svelte';
+	import Svelte from '$lib/icons/Svelte.icon.svelte';
 
-  onMount(() => {
-    // if ($theme === 'black') {
-      document.querySelector('html')?.setAttribute('data-theme', 'dracula')
-    // }
-  })
+	onMount(() => {
+		// if ($theme === 'black') {
+		document.querySelector('html')?.setAttribute('data-theme', 'dracula');
+		// }
+
+		console.log($page);
+	});
 </script>
 
 <nav>
-  <div class="container">
-    <div class="hidden md:visible"><h1>Todd Christensen</h1></div>
-    <div>
-      <ul>
-        <!-- {#if $page.path !== '/'} -->
-          <li><a href="/"><span class="logo">todd_christensen</span></a></li>
-        <!-- {/if} -->
+	<div class="container">
+		<div class="hidden md:visible"><h1>Todd Christensen</h1></div>
+		<div>
+			<ul>
+				{#if $page.url.pathname !== '/'}
+					<li><a href="/"><span class="logo">todd_christensen</span></a></li>
+				{/if}
 
-        <li><a href="/writing">Writing</a></li>
-        <li><a href="/reading">Reading</a></li>
-        <li><a href="/reading">Bookmarks</a></li>
-        <li><a class="flex flex-row items-center gap-2 normal-case" target="_blank" href="https://bytoddchristensen.com"><span class="text-base-content/70"><External /></span>byToddChristensen</a></li>
-        <li><ToggleTheme /></li>
-      </ul>
-    </div>
-  </div>
+				<li><a href="/writing">Writing</a></li>
+				<li><a href="/reading">Reading</a></li>
+				<li><a href="/reading">Bookmarks</a></li>
+				<li>
+					<a
+						class="flex flex-row items-center gap-2 normal-case"
+						target="_blank"
+						href="https://bytoddchristensen.com"
+						><span class="text-base-content/70"><External /></span>byToddChristensen</a
+					>
+				</li>
+				<li><ToggleTheme /></li>
+			</ul>
+		</div>
+	</div>
 </nav>
 
 <main>
-  <slot />
+	<slot />
 </main>
 
-<footer class="bg-base-200 flex items-center justify-center p-4 text-sm">Made with<a class="mx-1 text-[#ff3e00]" href="https://kit.svelte.dev/" target="_blank"> <Svelte /></a> in  🇵🇭  by a guy who is from a city near Cupertino, CA 🇺🇸.</footer>
+<footer class="bg-base-200 flex items-center justify-center p-4 text-sm">
+	Made with<a class="mx-1 text-[#ff3e00]" href="https://kit.svelte.dev/" target="_blank">
+		<Svelte /></a
+	> in 🇵🇭 by a guy who is from a city near Cupertino, CA 🇺🇸.
+</footer>
 
 <style lang="postcss">
-  .container {
-    @apply flex flex-row items-center justify-between pt-4;
-  }
-  h1 {
-    @apply hidden;
-  }
-  a {
-    @apply hover:text-accent-focus hover:underline;
-  }
-  .logo {
-    @apply font-bold;
-  }
-  nav ul {
-    @apply flex flex-row justify-around items-center list-none p-0 m-0 gap-8 font-semibold lowercase;
-    a {
-      @apply text-base-content;
-    }
-  }
+	.container {
+		@apply flex flex-row items-center justify-between pt-4;
+	}
+	h1 {
+		@apply hidden;
+	}
+	a {
+		@apply hover:text-accent-focus hover:underline;
+	}
+	.logo {
+		@apply font-bold;
+	}
+	nav ul {
+		@apply flex flex-row justify-around items-center list-none p-0 m-0 gap-4 sm:gap-8 font-semibold lowercase text-xs md:text-sm  md:text-base;
+		a {
+			@apply text-base-content;
+		}
+	}
 </style>
