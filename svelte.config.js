6a797f915f2e7f8e840ddba3dcd1a71ae77966ef
Todd Christensen <toddchristensen@protonmail.com>
2022-07-14 13:44:48 +0800

    fix: removed static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-cloudflare';
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-cloudflare';
 
 import preprocess from 'svelte-preprocess';
 
